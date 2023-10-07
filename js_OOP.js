534d97ce584d019e28aefaa39d95f70527d6fa77
mujahidtemp404 <mujahidislam400301@gmail.com>
2023-10-08 01:59:50 +0600

    some practice

---
+++ js_OOP.js
@@ -0,0 +1,84 @@
+// class developer{
+//     name;
+//     address;
+//     age;
+
+//     constructor(username,useraddress,userage){
+
+//         this.name=username;
+//         this.address=useraddress;
+//         this.age=userage;
+//     }
+//     projectSubmission(){
+//         console.log("shuld be submit the project before deadline");
+//     }
+//     attendent(){
+//         console.log("shuld be attendent before 09:00 AM");
+//     }
+// }
+
+// can't exucate-------------------------------
+
+// function btn(){
+//     const name=document.getElementById('name')
+//     const namev=name.value
+//     const age=document.getElementById('age')
+//     const agev=age.value
+//     const address=document.getElementById('address')
+//     const addressv=address.value
+//     console.log(namev,agev,addressv);
+// }
+// ------------------------------------------------
+// const mujahid=new developer();
+// console.log(mujahid);
+// mujahid.projectSubmission();
+// mujahid.attendent();
+
+// const dipu=new developer(${namev},${agev},${addressv});
+// console.log(dipu);
+
+
+
+// class student{
+//     name;
+//     roll;
+//     reg;
+//     address;
+//     age;
+//     classTime(){
+//         console.log('class will be start at 09:00 am');
+//     }
+//     classTimeEnad(){
+//         console.log('class will be end at 01:00 pm');
+//     }
+
+//     constructor(studentname,studentroll,studentreg,studentaddress,studentage){
+//         this.name=studentname;
+//         this.roll=studentroll;
+//         this.reg=studentreg;
+//         this.address=studentaddress;
+//         this.age=studentage;
+//     }
+// }
+
+// const mujahid=new student('mujahid',400314,1502038749,'Thakurgaon',22);
+// console.log(mujahid);
+// mujahid.classTime();
+// mujahid.classTimeEnad();
+
+
+// function Person(name, age, bloodGroup) {
+//     this.name = name;
+//     this.age = age;
+//     this.bloodGroup = bloodGroup;
+//   }
+
+//   const myFather = new Person("abc", 50, "AB+");
+//   const myMother = new Person("abc", 45, "B+");
+
+//   console.log('myFather Information',myFather);
+//   console.log('myMother Information',myMother);
+
+
+
+
